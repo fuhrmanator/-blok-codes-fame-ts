915dfba2c824702fe493f05fe9b0bed3964d97a4
JudeSeide <jude.seide@ssense.com>
2022-05-30 14:01:38 -0400

    json exporter

---
+++ app/Services/famix/FamixJSONExporter.ts
@@ -0,0 +1,58 @@
+import { BaseElement } from './BaseElement';
+import { isInstanceOf } from './helpers';
+
+interface FamixElement extends BaseElement {
+    id?: number;
+}
+
+export class FamixJSONExporter {
+    private element: FamixElement;
+    private readonly buffer: Record<string, unknown>;
+
+    constructor(clazz: string, element: FamixElement) {
+        this.element = element;
+
+        this.buffer = {
+            FM3: clazz,
+            id: this.element.id,
+        };
+    }
+
+    public readonly addProperty = (name: string, value: unknown): void => {
+        if (value === undefined || (value instanceof Set && value.size === 0)) {
+            return;
+        }
+
+        if (value instanceof Set) {
+            this.buffer[name] = this.addPropertiesFromSet(value);
+        } else if (this.isFamixElement(value)) {
+            this.buffer[name] = { ref: value.id };
+        } else if (typeof value === 'string') {
+            this.buffer[name] = value;
+        } else {
+            this.buffer[name] = value;
+        }
+    };
+
+    private readonly addPropertiesFromSet = (set: Set<unknown>): unknown[] => {
+        const properties: unknown[] = [];
+
+        set.forEach((value) => {
+            if (typeof value === 'string') {
+                properties.push(value);
+            } else if (this.isFamixElement(value)) {
+                properties.push({ ref: value.id });
+            } else {
+                properties.push(value);
+            }
+        });
+
+        return properties;
+    };
+
+    public readonly getJSON = (): string => JSON.stringify(this.buffer);
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    private readonly isFamixElement = (value: any): value is FamixElement =>
+        isInstanceOf<FamixElement>(value, ['getJSON', 'addPropertiesToExporter']);
+}
